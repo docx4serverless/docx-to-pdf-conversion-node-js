86e36ccea16a726ea8f6d1d137adeba81444c7fb
Florian Reuter <florian.reuter@docx-ai.com>
2020-06-19 12:30:21 +0200

    initial import

---
+++ docxToPdf/index.ts
@@ -0,0 +1,33 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { DocxWorker, DocxWorkerLogEntry } from '@docx-ai/docx4serverless';
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, docxAiService: any): Promise<void> {
+    const worker = await DocxWorker.createWorker(docxAiService);
+    try {
+        // import document and export as PDF:
+        await worker.loadDocument(context.req.body);
+        const pdf = await worker.getPDF();
+        context.res = {
+            status: 200,
+            isRaw: true,
+            contentType: "application/pdf",
+            body: pdf
+        };
+    } catch(e) {
+        context.res = {
+            status: 404,
+            isRaw: false,
+            contentType: "application/json",
+            body: {
+                error: e.toString(),
+                stack: e.stack
+            }
+        };
+    } finally {
+        worker.stop();
+    }
+
+    context.done();
+};
+
+export default httpTrigger;
